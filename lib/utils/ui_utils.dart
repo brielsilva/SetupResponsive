a92522d303c345dcc62c9169c61bcefc39f8413e
Gabriel Costa <gabrielcostasilva100@gmail.com>
2021-03-15 13:23:48 -0300

    refactor in the utils

--- lib/utils/ui_utils.dart
+++ lib/utils/ui_utils.dart
@@ -2,16 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:responsivesetup/enums/device_types.dart';
 
 DeviceTypes getDeviceTypes(MediaQueryData mediaQuery) {
-  var orientation = mediaQuery.orientation;
+  
 
-  double deviceWidth = 0;
-
-  if (orientation == Orientation.landscape) {
-    deviceWidth = mediaQuery.size.width;
-  } else {
-    deviceWidth = mediaQuery.size.width;
-  }
-
+  double deviceWidth = mediaQuery.size.shortestSide;
   if (deviceWidth > 950) {
     return DeviceTypes.Desktop;
   } else if (deviceWidth > 600) {
